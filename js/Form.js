d138c1f58e73c14484ac2e747ee808189e42d35c
ramya6492 <71939280+ramya6492@users.noreply.github.com>
2021-01-30 13:18:32 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,20 @@
+class Form {
+  constructor() {
+    
+  }
+
+  display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(130, 0);
+    
+    var input = createInput("Name");
+    var button = createButton('Play');
+    
+    input.position(130, 160);
+    button.position(250, 200);
+
+    button.mousePressed();
+
+  }
+}
